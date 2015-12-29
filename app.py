42bd132499980acab5a2fa8b83dc51dce48f3015
Thomas Apodaca <tma@angrymonkey.com>
2015-12-29 13:59:36 -0800

    org story requests are case insensitive

--- app.py
+++ app.py
@@ -302,7 +302,7 @@ def get_orgs_stories(organization_name):
         A cleaner url for getting an organizations stories
     '''
     # Check org name
-    organization = Organization.query.filter_by(name=raw_name(organization_name)).first()
+    organization = db.session.query(Organization).filter(func.lower(Organization.name) == func.lower(raw_name(organization_name))).first()
     if not organization:
         return "Organization not found", 404
 
